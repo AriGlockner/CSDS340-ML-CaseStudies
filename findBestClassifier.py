b3c26ad867eda05a4d59614ea585d9fb2b8616bf
Ari Glockner <ari@glockners.net>
2023-10-16 17:39:01 -0400

    Got something up and running to test the classifiers

--- findBestClassifier.py
+++ findBestClassifier.py
@@ -7,11 +7,16 @@ Things to do:
 5. Train and Test each model
 6. Get the best model
 """
+import math
+
 import numpy as np
 from sklearn.linear_model import Perceptron
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
 
 if __name__ == "__main__":
     # Read/Split the data from the file
@@ -21,22 +26,40 @@ if __name__ == "__main__":
     # TODO: Fix for a specific feature and label
     # TODO: Update with a loop to test all features and labels -> Maybe if not too big
     # TODO: Change 1st value in here to test a particular feature (0-29)
-    features = data[:, 0]
+    feature1 = 0
+    feature2 = 1
+    features = data[:, feature1:feature2]
     labels = data[:, -1]
 
     # Replace all -1 with the mean of the column
-    features = np.where(features == -1, np.nan, features)
-    mean = np.nanmean(np.where(features == -1, np.nan, features), axis=0)
+    mean = 0.0
+    number_of_values = 0
+    for i in range(len(features)):
+        if features[i] == -1:
+            features[i] = np.nan
+        else:
+            mean += features[i]
+            number_of_values += 1
 
-    print(features, 'n', labels)
+    mean /= number_of_values
+
+    for i in range(len(features)):
+        if np.isnan(features[i]):
+            features[i] = mean
+
+    '''features = np.where(features == -1, np.nan, features)
+    mean = np.nanmean(np.where(features == -1, np.nan, features), axis=0)'''
 
     # Split the data into train and test
     train_features, test_features, train_labels, test_labels = train_test_split(features, labels,
                                                                                 test_size=0.5, random_state=1)
 
     # Create a list of the ML models to be test
-    models = [Perceptron()]
-    model_labels = ['Perceptron']
+    C = 2.0
+    models = [Perceptron(), GaussianNB(), SVC(kernel='linear', C=C), SVC(kernel='poly', C=C), SVC(kernel='rbf', C=C),
+              SVC(kernel='sigmoid', C=C), DecisionTreeClassifier(), KNeighborsClassifier()]
+    model_labels = ['Perceptron', 'Naive Bayes', 'Adaline', 'Logistic Regression', 'SVM - Linear', 'SVM - poly',
+                    'SVM - rbf', 'SVM - sigmoid', 'Decision Tree', 'K-Nearest Neighbors']
     best_accuracy = 0
     best_model = 0
 
@@ -47,9 +70,10 @@ if __name__ == "__main__":
         model.fit(train_features, train_labels)
 
         # Test the model
-        testOutputs = model.predict_proba(test_features)[:, 1]
+        testOutputs = model.predict(test_features)
         aucTestRun = roc_auc_score(test_labels, testOutputs)
-        # print(f'Test set AUC: {aucTestRun}')
+        aucTestRun = max(aucTestRun, 1 - aucTestRun)
+        print(f'Test set AUC: {aucTestRun}')
 
         # Update the best model
         if aucTestRun > best_accuracy:
@@ -57,5 +81,6 @@ if __name__ == "__main__":
             best_model = i
 
     # Get the best model
-    # print(f'Best model: {model_labels[best_model]}')
-    # print(f'Best accuracy: {best_accuracy}')
+    print('\nFeature 1: ', feature1, '\tFeature 2: ', feature2)
+    print(f'Best model: {model_labels[best_model]}')
+    print(f'Best accuracy: {best_accuracy}')
